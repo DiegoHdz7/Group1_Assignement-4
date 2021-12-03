1a112a75aa3a643aad91a89c05f087890933fed4
Kass <Kass@DESKTOP-IGA1BK8>
2021-12-03 16:44:37 -0500

    updated

--- SavingAccount.cs
+++ SavingAccount.cs
@@ -6,19 +6,62 @@ using System.Threading.Tasks;
 
 namespace Group1_Assignement_4
 {
-    class SavingAccount : /*Account,*/ ITransaction
+    class SavingAccount: Account, ITransaction
     {
-        public void Deposit(double amount, Person person)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Withdraw(double amount, Person person)
-        {
-            throw new NotImplementedException();
-        }
-
+       
+        private static double COST_PER_TRANSACTION = 0.5;
+        private static double INTEREST_RATE = 0.015;
         
+        public SavingAccount (double balance = 0)
+            :base("SV-", balance)
+        {
+
+        }
+
+        public new void Deposit(double amount, Person person)
+        {
+            base.Deposit(amount, person);
+            base.OnTransactionOccur(null, null); //todo
+        }
+
+        public void Withdraw (double amount, Person person)
+        {
+            foreach (var item in base.users)
+            {
+                if(person.Name != item.Name)
+                {
+                    base.OnTransactionOccur(null, null);
+                    throw new AccountException(ExceptionEnum.NAME_NOT_ASSOCIATED_WITH_ACCOUNT);
+                }
+                else if (person.IsAuthenticated)
+                {
+                    base.OnTransactionOccur(null, null);
+                    throw new AccountException(ExceptionEnum.USER_NOT_LOGGED_IN);
+                }
+                else if (amount > base.Balance) //overdraft missing
+                {
+                    base.OnTransactionOccur(null, null);
+                    throw new AccountException(ExceptionEnum.NO_OVERDRAFT);
+                }
+                else
+                {
+                    base.OnTransactionOccur(null, null);
+                    base.Deposit(-amount, person);
+                }
+            }
+        }
+
+        public override void PrepareMonthlyReport()
+        {
+            int count = base.transactions.Count;
+            double serviceCharge = count * COST_PER_TRANSACTION;
+            double interest = LowestBalance * INTEREST_RATE / 12;
+            Balance += interest;
+            Balance -= serviceCharge;
+            transactions.Clear();
+        }
+
+
 
     }
 }
